313854aa7a23076291c7904dfadb968c953f1d62
Christoph Dörrer <d-chris@web.de>
2022-11-23 11:24:47 +0100

    renamed fixture to tmp_file
    added test to run script

--- test/test_pathutil.py
+++ test/test_pathutil.py
@@ -2,6 +2,7 @@ import pytest
 import hashlib
 import inspect
 import pathlib
+import subprocess
 
 from pathutil import PathUtil
 
@@ -9,7 +10,7 @@ CONTENT = 'foo\nbar!\n'
 
 
 @pytest.fixture()
-def my_file(tmp_path: pathlib.Path) -> str:
+def tmp_file(tmp_path: pathlib.Path) -> str:
     ''' returns a filename to a temporary testfile'''
     txt = tmp_path / 'test_file.txt'
 
@@ -17,16 +18,17 @@ def my_file(tmp_path: pathlib.Path) -> str:
     return str(txt)
 
 
-def test_eol_count(my_file):
-    p = PathUtil(my_file)
+def test_eol_count(tmp_file):
+    p = PathUtil(tmp_file)
     assert p.eol_count() == 2
+    assert p.eol_count(eol='\n') == 2
     assert p.eol_count(eol='\r') == 0
 
 
-def test_hexdigest(my_file):
-    p = PathUtil(my_file)
+def test_hexdigest(tmp_file):
+    p = PathUtil(tmp_file)
 
-    my_bytes = pathlib.Path(my_file).read_bytes()
+    my_bytes = pathlib.Path(tmp_file).read_bytes()
     md5 = hashlib.new('md5', my_bytes).hexdigest()
     sha1 = hashlib.new('sha1', my_bytes).hexdigest()
 
@@ -50,23 +52,30 @@ def test_available_algorithm():
         assert a in hashlib.algorithms_available
 
 
-def test_iter_lines(my_file):
+def test_iter_lines(tmp_file):
     with pytest.raises(FileNotFoundError):
         for line in PathUtil('file_not_available.txt').iter_lines():
             pass
 
-    my_generator = PathUtil(my_file).iter_lines()
+    my_generator = PathUtil(tmp_file).iter_lines()
 
     assert inspect.isgenerator(my_generator)
     assert list(my_generator) == str(CONTENT).splitlines()
 
 
-def test_iter_bytes(my_file):
+def test_iter_bytes(tmp_file):
     with pytest.raises(FileNotFoundError):
         for chunk in PathUtil('file_not_available.txt').iter_bytes():
             pass
 
-    my_generator = PathUtil(my_file).iter_bytes()
+    my_generator = PathUtil(tmp_file).iter_bytes()
 
     assert inspect.isgenerator(my_generator)
     assert list(my_generator)[0] == str(CONTENT).encode()
+
+
+def test_main():
+    ''' run script in virtual environment '''
+    p = subprocess.run('pipenv run src\\pathutil.py', shell=True)
+
+    assert p.returncode == 0
