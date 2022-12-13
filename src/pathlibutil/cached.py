c98384a0a202b87a2b7880014475f2b0a576b2ff
Christoph Dörrer <d-chris@web.de>
2022-12-13 15:35:07 +0100

    cache decorator for expensive funcs

--- src/pathlibutil/cached.py
+++ src/pathlibutil/cached.py
@@ -1,34 +1,47 @@
 from . import Path
-from functools import cache
+import functools
+
+
+def cache(func):
+    @functools.wraps(func)
+    def wrapper(self, *args, **kwargs):
+        try:
+            lock = (self.mtime, self)
+        except AttributeError:
+            lock = self
+
+        try:
+            func_cache = self.__cache[lock]
+        except (AttributeError, KeyError):
+            func_cache = dict()
+            self.__cache = {lock: func_cache}
+
+        try:
+            args_cache = func_cache[func.__name__]
+        except KeyError:
+            args_cache = dict()
+            self.__cache[lock][func.__name__] = args_cache
+
+        key = args + tuple(sorted(kwargs.items()))
+        try:
+            value = args_cache[key]
+        except KeyError:
+            value = func(self, *args, **kwargs)
+            args_cache[key] = value
+
+        return value
+
+    return wrapper
 
 
 class Path(Path):
 
-    def _check_cache_integrity(self):
-        ''' clear caches if file was modified '''
-        if self.modified:
-            self._cached_hexdigest.clear()
-            self._cached_eol_count.clear()
+    @cache
+    def hexdigest(
+        self, algorithm: str = None, *, size: int = None, length: int = None
+    ) -> str:
+        return super().hexdigest(algorithm=algorithm, size=size, length=length)
 
     @cache
-    def _cached_eol_count(self, **kwargs):
-        return super().eol_count(**kwargs)
-
-    @cache
-    def _cached_hexdigest(self, **kwargs):
-        return super().hexdigest(**kwargs)
-
-    def hexdigest(self, algorithm: str = None, *, size: int = None, length: int = None) -> str:
-        ''' return a cached hexdigest from the file '''
-        self._check_cache_integrity()
-
-        if algorithm:
-            algorithm = algorithm.casefold()
-
-        return self._cached_hexdigest(algorithm=algorithm, size=size, length=length)
-
     def eol_count(self, eol: str = None, size: int = None) -> int:
-        ''' return a cached eol count from the file '''
-        self._check_cache_integrity()
-
-        return self._cached_eol_count(eol=eol, size=size)
+        return super().eol_count(eol=eol, size=size)
